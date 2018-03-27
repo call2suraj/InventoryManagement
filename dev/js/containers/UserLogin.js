ee01246a0cb9a0fcbb4631e6ddd78f21ac493378
Suraj Behera <surajbehera@surajs-mbp.attlocal.net>
2018-03-27 11:28:44 -0500

    second commit

--- dev/js/containers/UserLogin.js
+++ dev/js/containers/UserLogin.js
@@ -1,10 +1,23 @@
 import React, {Component} from 'react';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import Divider from 'material-ui/Divider';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AppBar from 'material-ui/AppBar';
 import RaisedButton from 'material-ui/RaisedButton';
+import Checkbox from 'material-ui/Checkbox';
 import TextField from 'material-ui/TextField';
+
+const styles = {
+  block: {
+    maxWidth: 250,
+  },
+  checkbox: {
+    marginTop:15,
+    marginBottom: 16,
+    color:'gray'
+  },
+};
 class UserLogin extends Component {
 constructor(props){
   super(props);
@@ -16,52 +29,60 @@ constructor(props){
 render() {
     return (
       <div style={{textAlign:'left', backgroundColor:'whitesmoke',
-        borderWidth:1,width:550,height:380, borderRadius: '16px'}}>
+        borderWidth:1,width:620,height:400, borderRadius: '16px'}}>
 
         <MuiThemeProvider>
           <div style={{marginLeft:2}}>
-            <h2 style={{marginLeft:10}}>Login</h2>
-            <hr style={{margin:5}}/>
+            <h2 style={{marginLeft:10,paddingTop:15}}>Login</h2>
+            <Divider style={{margin:5}}/>
 
-            <div style={{marginLeft:22}}>
-            <TextField
-              hintText="Enter your Username"
-              floatingLabelText="Username" style={{width:430}}
-              onChange = {(event,newValue) => this.setState({username:newValue})}
-              />
-            <br/>
+            <div style={{marginLeft:22 }}>
               <TextField
-                type="password"
-                hintText="Enter your Password" style={{width:430}}
-                floatingLabelText="Password"
-                onChange = {(event,newValue) => this.setState({password:newValue})}
+                hintText="Enter your Username"
+                floatingLabelText="Username" style={{width:460}}
+                onChange = {(event,newValue) => this.setState({username:newValue})}
                 />
               <br/>
-              <SelectField
-                floatingLabelText="Domain"
-                value={this.state.value}
-                onChange={this.handleChange} style={{width:430}}
-              >
-                  <MenuItem value={1} primaryText="AR" />
-                  <MenuItem value={2} primaryText="BR" />
-                  <MenuItem value={3} primaryText="CA" />
-                  <MenuItem value={4} primaryText="CN" />
-                  <MenuItem value={5} primaryText="CR" />
+                <TextField
+                  type="password"
+                  hintText="Enter your Password" style={{width:460}}
+                  floatingLabelText="Password"
+                  onChange = {(event,newValue) => this.setState({password:newValue})}
+                  />
+                <br/>
+                <SelectField
+                  floatingLabelText="Domain"
+                  value={this.state.value}
+                  onChange={this.handleChange} style={{width:460}}
+                >
+                    <MenuItem value={1} primaryText="AR" />
+                    <MenuItem value={2} primaryText="BR" />
+                    <MenuItem value={3} primaryText="CA" />
+                    <MenuItem value={4} primaryText="CN" />
+                    <MenuItem value={5} primaryText="CR" />
 
 
-                  <MenuItem value={6} primaryText="GT" />
-                  <MenuItem value={7} primaryText="HN" />
-                  <MenuItem value={8} primaryText="HomeOffice" />
-                  <MenuItem value={9} primaryText="IN" />
-                  <MenuItem value={10} primaryText="JP" />
-                  <MenuItem value={11} primaryText="K2" />
-                  <MenuItem value={12} primaryText="MX" />
-              </SelectField>
+                    <MenuItem value={6} primaryText="GT" />
+                    <MenuItem value={7} primaryText="HN" />
+                    <MenuItem value={8} primaryText="HomeOffice" />
+                    <MenuItem value={9} primaryText="IN" />
+                    <MenuItem value={10} primaryText="JP" />
+                    <MenuItem value={11} primaryText="K2" />
+                    <MenuItem value={12} primaryText="MX" />
+                </SelectField>
+                <Checkbox
+                          label="Remember Me"
+                          style={styles.checkbox}
+                        />
 
-        <RaisedButton label="Login" primary={true} style={{margin: 15, width:330}}
-        onClick={(event) => this.handleClick(event)}/>
-        <br/>
-        <br/>
+                <div style={{marginTop:15, textAlign:'center' }}>
+                  <RaisedButton label="Login" primary={true} style={{ width:330, }}
+                  onClick={(event) => this.handleClick(event)}/>
+                </div>
+
+
+                <br/>
+                <br/>
 
             </div>
 
